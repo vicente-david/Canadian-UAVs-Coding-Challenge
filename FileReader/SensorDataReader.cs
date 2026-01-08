a0bf0fd4d3bf6a91b122e05d3de6a2a9d1a7267e
30172781 <vicente.david@ucalgary.ca>
2026-01-08 14:20:19 -0700

    finished algorithm to find sensors with anomalies. Implemented output, but not currently working as expected.

--- FileReader/SensorDataReader.cs
+++ FileReader/SensorDataReader.cs
@@ -5,6 +5,26 @@ using System.Collections.Generic;
 record SensorData(int Id, double Latitude, double Longitude); // record to store data for each sensor
 class SensorDataReader
 {
+    // =================== Helper Functions =======================
+    static double ToRad(double deg)
+    {
+        return deg * Math.PI / 180.0;
+    }
+    static double HaversineMeters(double lat1, double lon1, double lat2, double lon2)
+    {
+        // calculate the great-circle distance between two points on a sphere using Haversine formula. 
+        const double r = 6371000; // radius of the earth in meters
+        
+        var dLat = ToRad(lat2 - lat1);
+        var dLon = ToRad(lon2 - lon1);
+
+        var a = Math.Sin(dLat / 2) * Math.Sin(dLat / 2) + Math.Cos(ToRad(lat1)) * Math.Cos(ToRad(lat2)) * Math.Sin(dLon / 2) * Math.Sin(dLon / 2);
+        var c = 2 * Math.Atan2(Math.Sqrt(a), Math.Sqrt(1 - a));
+
+        return r * c; // returns the distance in meters
+    }
+    // ============================================================
+
     static void Main(string[] args)
     {
         if (args.Length != 2) // error check
@@ -31,21 +51,60 @@ class SensorDataReader
             csvSensors.Add(new SensorData(int.Parse(fields[0]), double.Parse(fields[1]), double.Parse(fields[2])));
         }
 
-        Console.WriteLine($"CSV Rows: {csvSensors.Count}");
         reader.Close();
         // ============================================================
 
         // =================== Read JSON sensor data ==================
         string jsonText = File.ReadAllText(jsonPath);
-        var jsonSensors = JsonSerializer.Deserialize<List<SensorData>>(jsonText);
+        var jsonSensors = JsonSerializer.Deserialize<List<SensorData>>(jsonText, new JsonSerializerOptions {PropertyNameCaseInsensitive = true});
 
         if (jsonSensors == null)
         {
             Console.WriteLine("Failed to read JSON sensor data.");
             return;
         }
+        // ============================================================
 
-        Console.WriteLine($"JSON Rows: {jsonSensors.Count}");
+
+        // =================== Evaluate Sensor Data ===================
+        List<(int csvId, int jsonId)> matches = new List<(int csvId, int jsonId)>();
+        const double MAX_DISTANCE = 100.0; // max distance of 100 meters as this is the accuracy of the sensors
+
+        foreach (var csvSensor in csvSensors)
+        {
+            foreach(var jsonSensor in jsonSensors)
+            {
+                double distance = HaversineMeters(csvSensor.Latitude, csvSensor.Longitude, jsonSensor.Latitude, jsonSensor.Longitude);
+
+                if (distance <= MAX_DISTANCE) // match found
+                {
+                    Console.WriteLine($"Match found.. CSV ID: {csvSensor.Id} JSON ID: {jsonSensor.Id}");
+                    matches.Add((csvSensor.Id, jsonSensor.Id)); // store matching ids
+                }
+            }
+        }
+        // ============================================================
+
+
+        // =================== Output Sensor Data =====================
+        // serialize matches to a formatted JSON string
+        string jsonString = JsonSerializer.Serialize(matches, new JsonSerializerOptions {WriteIndented = true });
+
+        // get file path to write the output to
+        string fileName = "output.json";
+        string filePath = Path.Combine(Directory.GetCurrentDirectory(), fileName);
+
+        // write the JSON string to the output directory
+        try
+        {
+            File.WriteAllText(filePath, jsonString);
+            Console.WriteLine($"Successfully Wrote JSON Output To: {filePath}");    
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine($"An Error Occured: {ex.Message}\n"); 
+        }
+        
         // ============================================================
     }
 }
